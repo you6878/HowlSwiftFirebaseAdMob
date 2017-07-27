0136df8a40aeffa1cc32fdb8bbf1b1c93926bb98
MyeongSic <you6878@icloud.com>
2017-07-27 21:03:33 +0900

    init

--- howladmob/ViewController.swift
+++ howladmob/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    var bannerView: GADBannerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        bannerView = GADBannerView(adSize: kGADAdSizeFullBanner)
+        self.view.addSubview(bannerView)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        bannerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        bannerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        
     }
 
     override func didReceiveMemoryWarning() {
